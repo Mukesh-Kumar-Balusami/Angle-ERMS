e882ea7d50e5896bdd191de0f6b7005471e80d58
Mukesh-Kumar-Balusami <balusamimukesh@gmail.com>
2024-07-24 23:10:54 +0530

    Connected to MongoDB Database - ERMSDB (Angle Software Company - ERMS)

--- api/index.js
+++ api/index.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=>{
+        console.log('DB Connected');
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+// mongoose
+// .connect("mongodb+srv://balusamimukesh:<password>@angle-erms-database.2w8z5np.mongodb.net/?retryWrites=true&w=majority&appName=Angle-ERMS-Database").then(()=>{
+//     console.log('Connected');
+// });
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(300, () => {
     console.log("Server is running on the port 3000...!!!...")
 });
